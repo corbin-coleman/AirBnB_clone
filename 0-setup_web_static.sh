a5da2f85f3b1da4964abdbf0794a6391a3995fcb
corbin-coleman <corbincol@hotmail.com>
2017-03-16 17:03:08 +0000

    Task 0 - Setup some directories and such

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install nginx -y
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+echo "Holberton School" | /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+if ! grep 'location /hbnb_static/' /etc/nginx/sites-enabled/default; then
+    sudo sed -i '29i\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-enabled/default
+fi
+sudo service nginx restart
